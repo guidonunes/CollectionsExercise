e04d569f133a1685a1956445afa3e07c0f617e3a
Guilherme Nunes <guilherme.augd@gmail.com>
2025-09-13 12:52:43 -0300

    basic ArrayList example

---
+++ src/ArrayListExample.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+
+        List<String> games = new ArrayList<>();
+
+        games.add("Baldur's Gate 3");
+        games.add("Alan Wake 2");
+        games.add("God of War");
+
+        System.out.println(games);
+    }
+}
