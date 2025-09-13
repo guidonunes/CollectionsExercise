468320347ad6c518f8c9d6c4a720c3ffb5837802
Guilherme Nunes <guilherme.augd@gmail.com>
2025-09-13 18:45:02 -0300

    exercise with ArrayList

--- src/VotingSystem.java
+++ src/VotingSystem.java
@@ -15,11 +15,35 @@ public class VotingSystem {
             System.out.println((i + 1) + ". " + films.get(i));
         }
 
-        System.out.println("\nType the number of the film of your choice (0 to end)");
+        System.out.println("\nType the number of the film of your choice (0 to end): ");
         int vote;
 
         do {
-
+            vote = sc.nextInt();
+            if (vote > 0 && vote <= films.size()) {
+                int index = vote - 1;
+                votes.set(index, votes.get(index) + 1);
+            } else if (vote != 0) {
+                System.out.println("Invalid option. Try again.");
+            }
         } while (vote!= 0);
+
+        int maxVotes = 0;
+        int winnerIndex = -1;
+
+        for (int i = 0; i < votes.size(); i++) {
+            if(votes.get(i) > maxVotes) {
+                maxVotes = votes.get(i);
+                winnerIndex = i;
+            }
+        }
+
+        if (winnerIndex != -1) {
+            System.out.println("\nThe winner is..." + films.get(winnerIndex) + " with " + maxVotes + " votes!");
+        } else {
+            System.out.println("\nNo films were voted.");
+        }
+        sc.close();
+
     }
 }
